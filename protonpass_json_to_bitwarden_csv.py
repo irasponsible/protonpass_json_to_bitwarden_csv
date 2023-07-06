88f772f48985a323a200b98787ae1d02bc524de3
irasponsible <138816927+irasponsible@users.noreply.github.com>
2023-07-07 05:07:57 +1000

    Update protonpass_json_to_bitwarden_csv.py
    Fixed typos in comments, added some more comments (still not good ones), added an input() to the end so you can see the printed messages

--- protonpass_json_to_bitwarden_csv.py
+++ protonpass_json_to_bitwarden_csv.py
@@ -1,13 +1,13 @@
-import json
 import csv
-from random import randint
+import json
+from random import randint  
 
-with open('data.json', 'r') as f:
+with open('data.json', 'r') as f:  # assume json is in the current directory
     pws = json.load(f)
 
 vaults = pws['vaults']
-export = []
-export_filename = f'export_{randint(0,10000)}.csv'
+export = []  # blank list used later to store all the passwords getting exported
+export_filename = f'export_{randint(0,10000)}.csv'  # generate a random number for the output csv filename
 print(export_filename)
 
 for vault in vaults.keys():
@@ -26,32 +26,34 @@ for vault in vaults.keys():
             print(metadata['name'], 'HAS NO CONTENT')
             continue
 
-        pw['name'] = metadata['name']           # Name
+        pw['name'] = metadata['name']                  # Name
 
         try:
-            pw['login_uri'] = content['urls'][0]   # URL
+            pw['login_uri'] = content['urls'][0]       # URL
         except IndexError:
             print(metadata['name'], 'HAS NO URLS')
             pw['login_uri'] = ''
 
         pw['login_username'] = content['username']     # Username
-        pw['login_password'] = content['password']      # Password
+        pw['login_password'] = content['password']     # Password
 
-        pw['folder'] = vaults[vault]['name']  # Folder (PP Vault Name)
-        pw['notes'] = metadata['note']  # Notes
+        pw['folder'] = vaults[vault]['name']          # Folder (uses PP vault name)
+        pw['notes'] = metadata['note']                # Notes
 
-        pw['type'] = 'login'
+        # assume everything is a login - i only had logins.
+        pw['type'] = 'login'  
         pw['favorite'] = 0
         pw['reprompt'] = 0
 
-        if data['extraFields']:
+        # stop extra fields exporting as '[]'
+        if data['extraFields']:  
             pw['fields'] = data['extraFields']
         else:
             pw['fields'] = ''
 
         if content['totpUri']:
             totp = content['totpUri'].split('?')[1].split('&')
-            # split on 0 leaves url and then the rest, split on & splits up the rest
+            # spliting on 0 leaves url and then the rest, split on & splits up the rest. then grab the one that has the secret
             for item in totp:
                 if item[0:7] == 'secret=':
                     pw['login_totp'] = item[7:]
@@ -60,17 +62,13 @@ for vault in vaults.keys():
 
         export.append(pw)
 
-# Expot to CSV
+# Export to CSV
 headers = ['name', 'login_uri', 'login_username', 'logn_password', 'folder', 'notes', 'type', 'favorite', 'reprompt', 'fields', 'login_totp']
 
 
 with open(export_filename, 'x', newline='') as expfile:
     csvw = csv.writer(expfile)
     csvw.writerow(headers)
-    #
-    # for item in export:
-    #     for field in item:
-    #         csvw.writerow(field)
 
     for item in export:
         x = []
@@ -78,3 +76,4 @@ with open(export_filename, 'x', newline='') as expfile:
             x.append(value)
         csvw.writerow(x)
 
+input('\n\nprogram finished, file saved', export_filename)
